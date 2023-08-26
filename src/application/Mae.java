b200f4c9b2d7b2a02edc22d87d883a1c85595293
Victor Souza <lordvcr@gmail.com>
2023-08-25 22:46:19 -0300

    Ratao

--- src/application/Mae.java
+++ src/application/Mae.java
@@ -3,5 +3,6 @@ package application;
 public class Mae {
     public static void main(String[] args) {
         System.out.println("Te amo mae");
+        System.out.println("Cindy ratao sujo");
     }
 }
