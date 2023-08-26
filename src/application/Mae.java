b45e67903a4c50b0b0b017021fb3e843de8a2a0a
Victor Souza <lordvcr@gmail.com>
2023-08-25 22:47:12 -0300

    Acabou o amor

--- src/application/Mae.java
+++ src/application/Mae.java
@@ -2,7 +2,6 @@ package application;
 
 public class Mae {
     public static void main(String[] args) {
-        System.out.println("Te amo mae");
         System.out.println("Cindy ratao sujo");
     }
 }
